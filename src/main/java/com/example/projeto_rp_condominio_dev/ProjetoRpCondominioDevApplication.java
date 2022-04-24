9570937229fe79775162015d70beb71840fff45a
Carlos Alberto Correa Gravi <89808076+carlosgravi@users.noreply.github.com>
2022-04-24 15:56:38 -0300

    iniciando o projeto

---
+++ src/main/java/com/example/projeto_rp_condominio_dev/ProjetoRpCondominioDevApplication.java
@@ -0,0 +1,13 @@
+package com.example.projeto_rp_condominio_dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoRpCondominioDevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoRpCondominioDevApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/projeto_rp_condominio_dev/ProjetoRpCondominioDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projeto_rp_condominio_dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoRpCondominioDevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
