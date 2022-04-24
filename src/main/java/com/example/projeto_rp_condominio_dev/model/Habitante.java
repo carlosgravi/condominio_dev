9356de4248994819cfe3adad4cefa4e7863c9f7c
Carlos Alberto Correa Gravi <89808076+carlosgravi@users.noreply.github.com>
2022-04-24 16:33:53 -0300

    criando entidade de habitante

---
+++ src/main/java/com/example/projeto_rp_condominio_dev/model/Habitante.java
@@ -0,0 +1,29 @@
+package com.example.projeto_rp_condominio_dev.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "habitantes")
+@Data
+public class Habitante {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "habitante_gen")
+    @SequenceGenerator(name = "habitante_gen", sequenceName = "habitantes_id_seq", allocationSize = 1)
+    private Long id;
+
+    private String nome;
+
+    private String sobrenome;
+
+    private String cpf;
+
+    @Column(name = "dt_nasc")
+    private LocalDate dtNasc;
+
+    private BigDecimal renda;
+}
