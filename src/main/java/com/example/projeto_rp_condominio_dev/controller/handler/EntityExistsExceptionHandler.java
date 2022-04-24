1f9546686f4e94cbea5632243999486789a9389d
Carlos Alberto Correa Gravi <89808076+carlosgravi@users.noreply.github.com>
2022-04-24 18:01:10 -0300

    adicionando exception handlers

---
+++ src/main/java/com/example/projeto_rp_condominio_dev/controller/handler/EntityExistsExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.example.projeto_rp_condominio_dev.controller.handler;
+
+import com.example.projeto_rp_condominio_dev.dto.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import javax.persistence.EntityExistsException;
+import java.time.LocalDateTime;
+
+@RestControllerAdvice
+public class EntityExistsExceptionHandler {
+    @ExceptionHandler({EntityExistsException.class})
+    public ResponseEntity<ErrorResponse> dateTimeParse(EntityExistsException e) {
+
+        ErrorResponse error = new ErrorResponse();
+        error.setCode(HttpStatus.BAD_REQUEST.value());
+        error.setTimestamp(LocalDateTime.now());
+        error.getMessages().add(e.getMessage());
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
+}
